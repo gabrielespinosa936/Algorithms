13b24e274b0a11573c6cc93ea49f8d26b8e6f30a
gabrielespinosa938 <Gabriel.espinosa936@myci.csuci.edu>
2019-03-03 23:31:56 -0800

    New file

---
+++ Solution.java
@@ -0,0 +1,63 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the diagonalDifference function below.
+    static int diagonalDifference(int[][] arr) {
+        int sizeOfArray = arr.length;
+        int a = 0;
+        int b = 0;
+        for(int i = 0; i < sizeOfArray; i++)
+        {
+            for(int j = 0; j < sizeOfArray; j++)
+            {
+                if(i==j)
+                {
+                    a+=arr[i][j];
+                }if(i == sizeOfArray-j-1)
+                {
+                    b += arr[i][j];
+                }
+            }
+        }
+        int result = Math.abs(a-b);
+        return result;
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[][] arr = new int[n][n];
+
+        for (int i = 0; i < n; i++) {
+            String[] arrRowItems = scanner.nextLine().split(" ");
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            for (int j = 0; j < n; j++) {
+                int arrItem = Integer.parseInt(arrRowItems[j]);
+                arr[i][j] = arrItem;
+            }
+        }
+
+        int result = diagonalDifference(arr);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
